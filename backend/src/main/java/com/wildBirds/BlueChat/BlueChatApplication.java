62ec823a465b8e6e679e80cff2be9c846436c21d
Igor <sowinski.igor@gmail.com>
2018-12-10 13:32:49 +0100

    first commit

---
+++ backend/src/main/java/com/wildBirds/BlueChat/BlueChatApplication.java
@@ -0,0 +1,12 @@
+package com.wildBirds.BlueChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueChatApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/wildBirds/BlueChat/BlueChatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wildBirds.BlueChat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlueChatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
