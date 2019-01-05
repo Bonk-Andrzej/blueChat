f9c34beeaca3e642c35908fb76b5ade648ccf04e
paweljastrzebski <paweljastrzebski96@gmail.com>
2019-01-05 14:51:33 +0100

    mobile: add router and backgroud-wrapper component

--- mobile/src/app/app.module.ts
+++ mobile/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import {AppRoutingModule} from "./app-routing/app-routing.module";
+import { BackgroundWrapperComponent } from './components/background-wrapper/background-wrapper.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        BackgroundWrapperComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ mobile/src/app/components/background-wrapper/background-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-background-wrapper',
+  templateUrl: './background-wrapper.component.html',
+  styleUrls: ['./background-wrapper.component.scss']
+})
+export class BackgroundWrapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
