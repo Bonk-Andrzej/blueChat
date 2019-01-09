3caf91b4248bc0ba913416ce903e6b92c92f650c
Igor <sowinski.igor@gmail.com>
2019-01-09 13:16:30 +0100

    mobile 1# refactor global move hamburger-menu

--- mobile/src/app/components/hamburger-menu/hamburger-menu.component.ts
+++ mobile/src/app/components/hamburger-menu/hamburger-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: '[app-hamburger-menu]',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HamburgerMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+
+    hamMenu() {
+        this.router.navigateByUrl('/home-menu');
+    }
 
 }
