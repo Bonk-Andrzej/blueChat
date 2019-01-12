852cc8fe3950917b8bffdf661153425f4e312526
Igor <sowinski.igor@gmail.com>
2019-01-12 17:07:17 +0100

    mobile add before add global

---
+++ mobile/src/app/components/box-profile/box-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box-profile',
+  templateUrl: './box-profile.component.html',
+  styleUrls: ['./box-profile.component.scss']
+})
+export class BoxProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
