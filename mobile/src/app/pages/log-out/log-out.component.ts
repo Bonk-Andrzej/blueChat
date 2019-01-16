6e66de5b2c8eee31c3a2f74a391547e037acc72d
paweljastrzebski <paweljastrzebski96@gmail.com>
2019-01-16 11:01:36 +0100

    reanme box-log-out to confrim-box and add added features to it, use confirm-box in log-out page and setup grid layout

--- mobile/src/app/pages/log-out/log-out.component.ts
+++ mobile/src/app/pages/log-out/log-out.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-log-out',
-  templateUrl: './log-out.component.html',
-  styleUrls: ['./log-out.component.scss']
+    selector: 'app-log-out',
+    templateUrl: './log-out.component.html',
+    styleUrls: ['./log-out.component.scss']
 })
 export class LogOutComponent implements OnInit {
 
-  constructor() { }
+    constructor(private router: Router) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    cancelHandler() {
+        this.router.navigateByUrl("/main-login").catch();
+    }
+
+    confirmHandler() {
+        this.router.navigateByUrl("/").catch();
+    }
 
 }
