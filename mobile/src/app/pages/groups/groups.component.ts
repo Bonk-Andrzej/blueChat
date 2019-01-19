dc4a1d691749a87adf459178743052ce365c0409
Igor <sowinski.igor@gmail.com>
2019-01-19 21:07:59 +0100

    mobile 1# add to groups

--- mobile/src/app/pages/groups/groups.component.ts
+++ mobile/src/app/pages/groups/groups.component.ts
@@ -1,5 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {FriendsDto} from '../../repository/friend/friendsDto';
+import {ChannelDtoShort} from '../../repository/channel/channelDtoShort';
+import {UserProfileComponent} from '../user-profile/user-profile.component';
+import {UserProfileService} from '../../services/user-profile.service';
 
 @Component({
     selector: 'app-groups',
@@ -8,19 +13,24 @@ import {Router} from '@angular/router';
 })
 export class GroupsComponent implements OnInit {
 
-    groups = [
-        {
-            'name': 'Java Poz 9',
-        },
-        {
-            'name': 'random',
-        },
-        {
-            'name': 'off topic',
-        }
-    ];
+    // groups = [
+    //     {
+    //         'name': 'Java Poz 9',
+    //     },
+    //     {
+    //         'name': 'random',
+    //     },
+    //     {
+    //         'name': 'off topic',
+    //     }
+    // ];
 
-    constructor(private router: Router) {
+    groups : Observable<Array<ChannelDtoShort>>;
+
+    constructor(private router: Router,
+                private userProfileService: UserProfileService) {
+
+        this.groups = this.userProfileService.getChannels();
     }
 
     ngOnInit() {
