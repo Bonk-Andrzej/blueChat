06367959b49fc295a827a3ac662de8642beb5f1d
Igor <sowinski.igor@gmail.com>
2019-01-23 23:35:45 +0100

    mobile 1# set temporrary remode enviromenth

--- mobile/src/app/environment.ts
+++ mobile/src/app/environment.ts
@@ -1,8 +1,9 @@
 
 
 export const environment = {
-    // host: "http://51.38.133.76:200",
+    host: "http://51.38.133.76:200",
     // host: "http://192.168.99.100:200",
-    host : "http://localhost:8080",
-    socketHost : "ws://localhost:8080"
+    // host : "http://localhost:8080",
+    // socketHost : "ws://localhost:8080"
+    socketHost : "ws://51.38.133.76:200"
 }
