aa7b4f82bcd27b759014dce26a4745c427350edb
Igor <sowinski.igor@gmail.com>
2019-03-31 16:05:01 +0200

    mobile 1# finishet settings and set default settings

--- mobile/src/app/services/own-emoji-service.service.ts
+++ mobile/src/app/services/own-emoji-service.service.ts
@@ -1,20 +1,50 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {EmojiData} from '@ctrl/ngx-emoji-mart/ngx-emoji';
+import {LocalStorageService} from './local-storage.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class OwnEmojiServiceService {
 
-
-    private cuttentType: BehaviorSubject<EmojiType>;
+    private readonly STORAGEEMOJITYPE = 'StorageEmojiType';
+    private readonly STOGRAGEEMOJIQUALITY = 'StorageEmojiQuality';
+    private cuttentType : BehaviorSubject<EmojiType>;
     private currentQuality: BehaviorSubject<string>;
     public onSelectetEmoji = new EventEmitter<string>();
 
-    constructor() {
+    constructor(private storage: LocalStorageService) {
         this.cuttentType = new BehaviorSubject<EmojiType>(EmojiType.TWITTER);
         this.currentQuality = new BehaviorSubject<string>('LOW')
+        // this.checkStorageQuality();
+        // this.checkStorageType();
+    }
+
+    private checkStorageType() {
+        if (this.storage.hasObject(this.STORAGEEMOJITYPE)) {
+            let storageType = this.storage.fetchObject(this.STORAGEEMOJITYPE, String);
+            this.cuttentType.next(EmojiType[storageType.valueOf()]);
+            this.storage.saveObject(this.STORAGEEMOJITYPE, storageType);
+        } else {
+            this.cuttentType.next(EmojiType.TWITTER);
+        }
+
+
+    }
+
+    private checkStorageQuality() {
+        if (this.storage.hasObject(this.STOGRAGEEMOJIQUALITY)) {
+            let quality = this.storage.fetchObject(this.STOGRAGEEMOJIQUALITY, String);
+
+            let stringObject = new String(quality);
+            let stringPrimitive = stringObject.valueOf();
+
+            this.currentQuality.next(stringPrimitive);
+            this.storage.saveObject(this.STOGRAGEEMOJIQUALITY, stringObject);
+        } else {
+            this.currentQuality.next('LOW');
+        }
     }
 
     //type
@@ -25,21 +55,26 @@ export class OwnEmojiServiceService {
     public setTupe(type: EmojiType) {
         this.cuttentType.next(type);
     }
-    public getTypeObs() : Observable<EmojiType>{
-        return this.cuttentType.asObservable()
+
+    public getTypeObs(): Observable<EmojiType> {
+        return this.cuttentType.asObservable();
     }
-    public getType() : EmojiType{
-        return this.cuttentType.getValue()
+
+    public getType(): EmojiType {
+        return this.cuttentType.getValue();
     }
+
     //quality
-    public setQuality(quality: string){
+    public setQuality(quality: string) {
         this.currentQuality.next(quality);
     }
-    public getQualityObs(): Observable<string>{
+
+    public getQualityObs(): Observable<string> {
         return this.currentQuality.asObservable();
     }
-    public getQuality() : EmojiQuality {
-        console.error(this.currentQuality.getValue())
+
+    public getQuality(): EmojiQuality {
+        console.error(this.currentQuality.getValue());
         return EmojiQuality[this.currentQuality.getValue()];
     }
 
@@ -47,18 +82,17 @@ export class OwnEmojiServiceService {
 }
 
 export enum EmojiType {
-
     GOOGLE = 'google',
     APPLE = 'apple',
     TWITTER = 'twitter',
     EMOJIONE = 'emojione',
     MESSAGER = 'messenger',
     FACEBOOK = 'facebook',
-
 }
 
 export enum EmojiQuality {
-    POOR= 16,
+    POOR = 16,
     LOW = 20,
     GOOD = 32,
+    HIGH = 64,
 }
