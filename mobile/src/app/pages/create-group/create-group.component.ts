dcb5b92ca948c4b3738bdd2f2552bdef4ff9ebc0
Igor <sowinski.igor@gmail.com>
2019-04-02 21:06:11 +0200

    mobile 1# set shape of page

--- mobile/src/app/pages/create-group/create-group.component.ts
+++ mobile/src/app/pages/create-group/create-group.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ColorsService} from '../../services/colors.service';
-import {UserProfileService} from "../../services/user-profile.service";
-import {Observable} from "rxjs";
-import {FriendsObs} from "../../services/model/friendsObs";
+import {UserProfileService} from '../../services/user-profile.service';
+import {Observable} from 'rxjs';
+import {FriendsObs} from '../../services/model/friendsObs';
 
 @Component({
     selector: 'app-create-group',
@@ -13,13 +13,15 @@ export class CreateGroupComponent implements OnInit {
 
     colorButton: string;
     colorTextOnButton: string;
-    image: string;
+    // image: string;
+    confirmStatus: string;
     public listToCreate: Array<FriendsObs> = new Array<FriendsObs>();
 
     public form = {
-        groupName: "",
-        description: ""
-
+        groupName: '',
+        description: '',
+        public: true,
+        userListId: []
     };
 
     public friends: Observable<Array<FriendsObs>>;
@@ -33,30 +35,71 @@ export class CreateGroupComponent implements OnInit {
     ngOnInit() {
         this.colorButton = this.colorService.getColor('--orange');
         this.colorTextOnButton = this.colorService.getColor('--white-light');
-        this.friends = this.userProfileService.getFriends()
-        let x = true;
-        this.image = `url(assets/checkbox-none.svg)`;
-        // setInterval(()=>{
-        //
-        //     if(x){
-        //         this.image = `url(assets/checkbox-none.svg)`;
-        //     }else{
-        //         this.image = `url(assets/checkbox-accept.svg)`;
-        //     }
-        //     x = !x;
-        //
-        // },500)
+        this.friends = this.userProfileService.getFriends();
+        this.confirmStatus = 'none';
+        // this.image = `url(assets/checkbox-none.svg)`;
     }
 
-    addToList(friend: FriendsObs) {
+    //settings current list
+    addToList(friend: FriendsObs): void {
         this.listToCreate.push(friend);
     }
 
-    removeFromList(friendToRemove: FriendsObs) {
-        this.listToCreate = this.listToCreate.filter(friend => friend !== friendToRemove)
+    removeFromList(friendToRemove: FriendsObs): void {
+        this.listToCreate = this.listToCreate.filter(friend => friend !== friendToRemove);
     }
 
-    setPublic(isPublic: boolean) {
+    //settings status public / private
+    changeRange(isPublic: boolean): void {
+        this.form.public = isPublic;
+    }
+
+    // settings accepting create group
+    createGroup(): void {
+        console.log('LISTA FRIENDSOW' + this.listToCreate);
+        console.log('LISTA userow z formularza' + this.form.userListId);
+
+
+        if (this.validateFields() != false) {
+            this.addSelectedUsers(this.listToCreate);
+            this.confirmStatus = 'block';
+        } else {
+            alert('Brakuje czegos');
+        }
+    }
+
+    //setting confirm status
+    onCancel(): void {
+        this.confirmStatus = 'none';
 
     }
+
+    onConfirmEdit(): void {
+        console.log('UDALO CI SIE ');
+        this.clearForm();
+    }
+
+
+    private validateFields(): boolean {
+        if ((this.form.groupName == '') || (this.form.description == '') || (this.listToCreate.length == 0)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private addSelectedUsers(friendsList: Array<FriendsObs>) {
+        friendsList.forEach(friend => {
+            this.form.userListId.push(friend.getFriend().getIdUser());
+        });
+    }
+
+    private clearForm(): void {
+        this.form.groupName = '';
+        this.form.description = '';
+        this.changeRange(true);
+        this.form.userListId = [];
+        this.listToCreate = new Array<FriendsObs>();
+    }
+
 }
